2055cfe793dc790f73da71c7f9b13c9b69299457
xuehuichao <xue.huichao@gmail.com>
2016-11-09 15:04:04 -0800

    Added documentations to the script

--- compile_beamer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-python convert.py --beamer_latex_file=mindmap.tex --mindmap_file=mindmap.mm
-pdflatex -interaction=nonstopmode slides.tex
-bibtex slides
-pdflatex -interaction=nonstopmode slides.tex
-pdflatex -interaction=nonstopmode slides.tex
--- inotify_beamer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TIME_FORMAT='%F %H:%M'
-OUTPUT_FORMAT='%T Event(s): %e fired for file: %w. Refreshing.'
-while inotifywait -q -r --timefmt "${TIME_FORMAT}" --format "${OUTPUT_FORMAT}" mindmap.mm slides.tex convert.py; do
-    ./compile_beamer.sh
-done
